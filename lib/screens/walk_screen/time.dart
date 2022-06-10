8a3bd20f156a07855fdcc0ee45b46920b0a2ebbe
yeaji <dpwl0974@naver.com>
2022-06-10 16:17:32 +0900

    y_commit(1)

--- lib/screens/walk_screen/time.dart
+++ lib/screens/walk_screen/time.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:walking_test/main.dart';
+//import 'package:walking_test/main.dart';
 import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:numberpicker/numberpicker.dart';
