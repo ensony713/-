e22f522f9eecb891e080fd463b63502ed1cb34e4
Yeaji Yun <dpwl0974@naver.com>
2022-06-20 19:50:10 +0900

    y_commit(0620)

--- lib/screens/diary.dart
+++ lib/screens/diary.dart
@@ -1,9 +1,13 @@
 
+import 'dart:collection';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:bubble_box/bubble_box.dart';
+import 'dart:collection';
+import '../DB/Recode.dart';
 
 class Diary extends StatefulWidget {
   @override
@@ -12,7 +16,6 @@ class Diary extends StatefulWidget {
 }
 
 class _DiaryState extends State<Diary> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -42,6 +45,7 @@ class _DiaryState extends State<Diary> {
                     _pageOfMiddle(),
                     //-pageOfBottom(),
                   ],
+
                 )
             )
         )
@@ -62,6 +66,13 @@ Widget _pageOfTop() {
     calendarStyle: CalendarStyle(todayDecoration: BoxDecoration(color: Colors.transparent,shape: BoxShape.circle, border: Border.all(color: Colors.lightGreen, width: 1.5/*image: DecorationImage(image: AssetImage('images/icons/putprint.png')*/)),
         todayTextStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)
     ),
+    /*eventLoader: (day){
+      if(day.day%2==0) {
+        return ['g'];
+      }
+      return
+        [];
+    },*/
   );
 }
 
@@ -77,6 +88,87 @@ Widget _pageOfMiddle() {
       direction: BubbleDirection.left,
     ),
     backgroundColor: Colors.transparent,
-    child: Text('이번 달 산책 횟수, 목적지 산책 거리, 시간 산책 시간 표시 \n\n\n'),
+
+    child: Text("오늘 시간 산책은 " '$_len' " 했구요" "\n"
+        "오늘 목적지 산책은 " '$_time' "했어요!"),
   );
+}
+
+
+
+/*void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}*/
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Open route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+int _len = 0;
+int _time = 0;
+String date = "";
+String day = '';
+
+String getDate() {
+  DateTime now = DateTime.now();
+  DateFormat format = DateFormat('yyyy/MM/dd');
+  date = format.format(now);
+
+  DateFormat dateFormat = DateFormat('yyyy년 MM월 dd일');
+  day = dateFormat.format(now);
+  return date;
+}
+
+Future<int> getLength() async {
+  Recode recode = await getDateRecode(date);
+  _len = recode.length;
+
+  return _len;
+}
+
+Future<int> getTime() async {
+  Recode recode = await getDateRecode(date);
+  _time = recode.time;
+  _time.toString();
+
+  return _time;
 }
